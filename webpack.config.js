4f23a8da1ca309b62e7a8a02d8b92565b938e381
Evgenii Latyshev <evgeniy.latishev@yandex.ru>
2021-09-22 09:20:10 +0400

    Added eslint config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,35 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProd = process.env.NODE_ENV === 'production';
 const isDev = !isProd;
 
 const fileName = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
 
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  ]
+
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+
+  return loaders
+}
+
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  entry: ["@babel/polyfill", "./index.js"],
+  entry: ['@babel/polyfill', './index.js'],
   output: {
     filename: fileName('js'),
     path: path.resolve(__dirname, 'dist')
@@ -41,7 +58,8 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src/favicon.ico'), to: path.resolve(__dirname, 'dist')
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist')
         }
       ],
     }),
@@ -55,18 +73,17 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
+          'css-loader',
+          'sass-loader',
         ],
       },
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-env']
-          }
+        use: jsLoaders(),
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
         }
       }
     ],
